56628bcda542b82d56e6fb0491405bf50903e1a7
Apo <apoorva.shettigar@spin.pm>
2025-01-03 01:39:16 -0800

    moved sqls

--- 175-combine-two-tables/combine-two-tables.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-select 
-firstname, 
-lastname,
-city,
-state
-from person p
-left join address a on p.personid = a.personid
--- 183-customers-who-never-order/customers-who-never-order.sql
+++
@@ -1,5 +0,0 @@
-# Write your MySQL query statement below
-select name as Customers
-from customers c
-left join orders o on o.customerid = c.id
-where o.id is null
