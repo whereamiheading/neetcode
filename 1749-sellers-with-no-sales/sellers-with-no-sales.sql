22ea7a25c8fe8ee190166c9d7e0541138203cc56
whereamiheading <32666156+whereamiheading@users.noreply.github.com>
2025-01-02 22:36:19 -0800

    Time: 2091 ms (7.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select seller_name
+from seller where seller_id not in 
+(select 
+seller_id
+from 
+orders
+where  year(sale_date) = 2020)
+order by seller_name asc
